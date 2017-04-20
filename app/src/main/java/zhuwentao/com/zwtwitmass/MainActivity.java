2712a2bcd73f4c22c3a2df3673746706522a9d75
zhuwentao <215069587@qq.com>
2017-04-20 23:15:19 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/MainActivity.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/MainActivity.java
@@ -1,6 +1,8 @@
 package zhuwentao.com.zwtwitmass;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,26 +13,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-import zhuwentao.com.zwtwitmass.uimodule.HorizontalGridViewAct;
-import zhuwentao.com.zwtwitmass.uimodule.HorizontalGridViewLineAct;
-import zhuwentao.com.zwtwitmass.uimodule.TextViewActivity;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 首页
  */
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button mTextSizeBtn;
-    private Button mHorizontalGridViewBtn;
-    private Button mHorizontalGridViewLineBtn;
+    private ListView mDataLv;
 
     private Toolbar mTitleBar;
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private ActionBarDrawerToggle toggle;
+
+
+
+    private List<Map<String, Object>> mDatas;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +53,41 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(mTitleBar);
 
         initListener();
+
+
+        initData();
     }
 
-    public void initUI(){
+    private void initData(){
+        Intent intent = getIntent();
+        String path = intent.getStringExtra("com.example.android.apis.Path");
+
+        if (path == null) {
+            path = "";
+        }
+
+        mDatas = getData(path);
+        mDataLv.setAdapter(new SimpleAdapter(this, mDatas,
+                android.R.layout.simple_list_item_1, new String[] { "title" },
+                new int[] { android.R.id.text1 }));
+
+        mDataLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, Object> map = mDatas.get(position);
+
+                Intent intent = (Intent) map.get("intent");
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void initUI() {
         mTitleBar = (Toolbar) findViewById(R.id.toolbar);
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
-        mTextSizeBtn = (Button) findViewById(R.id.btn_text_size);
-        mHorizontalGridViewBtn = (Button) findViewById(R.id.btn_text_horizontal_gridview);
-        mHorizontalGridViewLineBtn = (Button) findViewById(R.id.btn_text_horizontal_gridview_line);
+
+        mDataLv = (ListView) findViewById(R.id.lv_main_demo);
 
         toggle = new ActionBarDrawerToggle(
                 this, drawer, mTitleBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -57,12 +95,8 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
     }
 
-    public void initListener(){
+    public void initListener() {
         navigationView.setNavigationItemSelectedListener(this);
-        mTextSizeBtn.setOnClickListener(this);
-        mHorizontalGridViewBtn.setOnClickListener(this);
-        mHorizontalGridViewLineBtn.setOnClickListener(this);
-
     }
 
 
@@ -123,22 +157,94 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = null;
-        switch (v.getId()){
-            case R.id.btn_text_size:
-                intent = new Intent(MainActivity.this, TextViewActivity.class);
-                break;
-            case R.id.btn_text_horizontal_gridview:
-                intent = new Intent(MainActivity.this, HorizontalGridViewAct.class);
-                break;
-            case R.id.btn_text_horizontal_gridview_line:
-                intent = new Intent(MainActivity.this, HorizontalGridViewLineAct.class);
-                break;
+    protected List<Map<String, Object>> getData(String prefix) {
+        List<Map<String, Object>> myData = new ArrayList<Map<String, Object>>();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory("android.intent.category.ZWT_SAMPLE_CODE");
+
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
+
+        if (null == list)
+            return myData;
+
+        String[] prefixPath;
+        String prefixWithSlash = prefix;
+
+        if (prefix.equals("")) {
+            prefixPath = null;
+        } else {
+            prefixPath = prefix.split("/");
+            prefixWithSlash = prefix + "/";
         }
-        if(intent != null){
-            startActivity(intent);
+
+        int len = list.size();
+
+        Map<String, Boolean> entries = new HashMap<String, Boolean>();
+
+        String currPkgName = this.getPackageName();
+        String targetPkgName;
+        for (int i = 0; i < len; i++) {
+            ResolveInfo info = list.get(i);
+            targetPkgName = info.activityInfo.applicationInfo.packageName;
+            if (currPkgName.equals(targetPkgName)) {
+                CharSequence labelSeq = info.loadLabel(pm);
+                String label = labelSeq != null
+                        ? labelSeq.toString()
+                        : info.activityInfo.name;
+
+                if (prefixWithSlash.length() == 0 || label.startsWith(prefixWithSlash)) {
+
+                    String[] labelPath = label.split("/");
+
+                    String nextLabel = prefixPath == null ? labelPath[0] : labelPath[prefixPath.length];
+
+                    if ((prefixPath != null ? prefixPath.length : 0) == labelPath.length - 1) {
+                        addItem(myData, nextLabel, activityIntent(
+                                info.activityInfo.applicationInfo.packageName,
+                                info.activityInfo.name));
+                    } else {
+                        if (entries.get(nextLabel) == null) {
+                            addItem(myData, nextLabel, browseIntent(prefix.equals("") ? nextLabel : prefix + "/" + nextLabel));
+                            entries.put(nextLabel, true);
+                        }
+                    }
+                }
+            }
         }
+
+        Collections.sort(myData, sDisplayNameComparator);
+
+        return myData;
+    }
+
+    protected void addItem(List<Map<String, Object>> data, String name, Intent intent) {
+        Map<String, Object> temp = new HashMap<String, Object>();
+        temp.put("title", name);
+        temp.put("intent", intent);
+        data.add(temp);
+    }
+
+    private final static Comparator<Map<String, Object>> sDisplayNameComparator =
+            new Comparator<Map<String, Object>>() {
+                private final Collator collator = Collator.getInstance();
+
+                public int compare(Map<String, Object> map1, Map<String, Object> map2) {
+                    return collator.compare(map1.get("title"), map2.get("title"));
+                }
+            };
+
+    protected Intent activityIntent(String pkg, String componentName) {
+        Intent result = new Intent();
+        result.setClassName(pkg, componentName);
+        return result;
+    }
+
+    protected Intent browseIntent(String path) {
+        Intent result = new Intent();
+        result.setClass(this, MainActivity.class);
+        result.putExtra("com.example.android.apis.Path", path);
+        return result;
     }
 }

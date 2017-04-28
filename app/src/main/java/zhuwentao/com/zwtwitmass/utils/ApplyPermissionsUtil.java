a63e7571ac3b3407266a141d6ee84c7f0b977c04
zhuwentao <215069587@qq.com>
2017-04-28 21:24:55 +0800

    usually commit

---
+++ app/src/main/java/zhuwentao/com/zwtwitmass/utils/ApplyPermissionsUtil.java
@@ -0,0 +1,71 @@
+package zhuwentao.com.zwtwitmass.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+
+/**
+ * 权限检查和申请
+ * Created by zhuwentao on 2017-04-28.
+ */
+public class ApplyPermissionsUtil {
+
+    /**
+     * 检查是否有该权限
+     * true=有权限 false=无权限
+     */
+    public static boolean checkHavePermissions(Context context, String permission) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (getTargetSdkVersion(context) >= 23) {
+                LogUtil.e("执行ContextCompat检查");
+                return ContextCompat.checkSelfPermission(context, permission)
+                        == PackageManager.PERMISSION_GRANTED;
+            } else {
+                LogUtil.e("执行PermissionChecker检查");
+                return PermissionChecker.checkSelfPermission(context, permission)
+                        == PackageManager.PERMISSION_GRANTED;
+            }
+        }
+        LogUtil.e("SDK_INT小于23");
+        return true;
+    }
+
+    /**
+     * 权限校验
+     */
+    public static void checkRequestPermissions(Context context, String permission, int requestCode) {
+        if (permission == null) {
+            return;
+        }
+        boolean result = checkHavePermissions(context, permission);
+        if (!result) {
+            LogUtil.e("无权限" + permission);
+            // 弹出对话框申请权限
+            // 最后一个参数标识该申请权限，可以在Activity界面通过onRequestPermissionsResult回调方法来监听权限申请的结果
+            ActivityCompat.requestPermissions((Activity) context, new String[]{permission}, requestCode);
+        } else {
+            LogUtil.e("有权限" + permission);
+        }
+    }
+
+    /**
+     * 获取应用targetSdkVersion
+     * @param context 上下文
+     * @return  返回应用targetSdkVersion
+     */
+    private static int getTargetSdkVersion(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0);
+            return info.applicationInfo.targetSdkVersion;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}

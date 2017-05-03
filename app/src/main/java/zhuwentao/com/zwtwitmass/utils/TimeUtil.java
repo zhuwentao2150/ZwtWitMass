9dc1552e0dfee31b1093905cc712aa7fa4e00fd7
zhuwentao <215069587@qq.com>
2017-05-03 22:15:39 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/utils/TimeUtil.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/utils/TimeUtil.java
@@ -2,6 +2,7 @@ package zhuwentao.com.zwtwitmass.utils;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.text.ParseException;
@@ -97,6 +98,117 @@ public class TimeUtil {
             }
         }
         return str;
+    }
+
+    /**
+     * 判断与当前系统时间间隔是昨天还是前天或者是其他
+     *
+     * @param ccTime           时间参数
+     * @param simpleDateFormat 日期时间格式
+     * @return boolean 时间日期是否是昨天/前天
+     */
+    private static boolean isYesterdayOrBefore(long ccTime, SimpleDateFormat simpleDateFormat, StringBuffer time) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add( Calendar.DAY_OF_MONTH, -1 );
+        if (simpleDateFormat.format( calendar.getTime() ).equals( simpleDateFormat.format( ccTime ) )) {
+            /*
+             * LogUtil.d(TAG, "昨天----------------" + new SimpleDateFormat("yyyy-MM-dd",Locale.getDefault()) .format(calendar.getTime()));
+             */
+            time.append( "昨天" );
+            return true;
+        } else {
+            calendar.clear( Calendar.DAY_OF_MONTH );
+            calendar.add( Calendar.DAY_OF_MONTH, -1 );
+            if (simpleDateFormat.format( calendar.getTime() ).equals( simpleDateFormat.format( ccTime ) )) {
+                /*
+                 * LogUtil.d(TAG, "前天_________-----" + new SimpleDateFormat("yyyy-MM-dd",Locale.getDefault()) .format(calendar.getTime()));
+                 */
+                time.append( "前天" );
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断当前日期是否过期
+     *
+     * @param dateStr 截至日期
+     * @return true:已过期，false:未过期。
+     */
+    public static boolean isTodayOverDue(String dateStr) {
+        SimpleDateFormat sdf = new SimpleDateFormat( DATE_FORMAT_DEFAULT, Locale.getDefault() );
+        try {
+            String todayDateStr = sdf.format( new Date() );
+            Date todayDate = sdf.parse( todayDateStr );
+            Date comparedDateDate = sdf.parse( dateStr );
+            return todayDate.getTime() > comparedDateDate.getTime();
+        } catch (ParseException e) {
+            Log.e( TAG, "isTodayOverDue() " + e.getMessage(), e );
+        }
+        return false;
+    }
+
+    /**
+     * 删除时间格式后面的毫秒
+     *
+     * @param time 时间 格式：yyyy-MM-dd HH:mm:ss.SSS
+     * @return 删除毫秒的时间 格式：yyyy-MM-dd HH:mm:ss
+     */
+    public static String delMillisecond(String time) {
+        String newTime = time;
+        if (!TextUtils.isEmpty( newTime )) {
+            int index = newTime.indexOf( "." );
+            if (index > 0) {
+                newTime = newTime.substring( 0, index );
+            }
+        }
+        return newTime;
+    }
+
+    /**
+     * 将秒转成时分秒格式
+     *
+     * @param time 时间（ms）
+     * @return 返回 时：分 ：秒
+     */
+    public static String secToTime(int time) {
+        String timeStr = null;
+        int hour = 0;
+        int minute = 0;
+        int second = 0;
+        if (time <= 0)
+            return "00:00:00";
+        else {
+            minute = time / 60;
+            if (minute < 60) {
+                second = time % 60;
+                timeStr = "00:" + unitFormat( minute ) + ":" + unitFormat( second );
+            } else {
+                hour = minute / 60;
+                if (hour > 99)
+                    return "00:00:00";
+                minute = minute % 60;
+                second = time - hour * 3600 - minute * 60;
+                timeStr = unitFormat( hour ) + ":" + unitFormat( minute ) + ":" + unitFormat( second );
+            }
+        }
+        return timeStr;
+    }
+
+    /**
+     * 将整数转成字符串
+     *
+     * @param i 时间
+     * @return 格式化后的时间
+     */
+    public static String unitFormat(int i) {
+        String retStr = null;
+        if (i >= 0 && i < 10)
+            retStr = "0" + Integer.toString( i );
+        else
+            retStr = "" + i;
+        return retStr;
     }
 
 }

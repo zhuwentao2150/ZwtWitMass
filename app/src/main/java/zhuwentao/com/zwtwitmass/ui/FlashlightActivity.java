c0f740c7a9e692f0e7d13b1afe3f1215b394d74a
zhuwentao <215069587@qq.com>
2017-05-29 21:11:37 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/ui/FlashlightActivity.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/ui/FlashlightActivity.java
@@ -1,10 +1,13 @@
 package zhuwentao.com.zwtwitmass.ui;
 
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import zhuwentao.com.zwtwitmass.R;
 
 /**
  * 手电筒
@@ -19,6 +22,35 @@ public class FlashlightActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mButtonOpenOrClose = (Button) findViewById(R.id.btn_open_flashlight);
+        camera = Camera.open();
+        isFlash = true;
+    }
+    /**
+     * 操作手电筒
+     * @param v
+     */
+    public void openOrClose(View v){
+        if(isFlash){
+            Camera.Parameters mParameters = camera.getParameters();
+            // 更改手电筒的状态
+            mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(mParameters);
+            mButtonOpenOrClose.setText("关闭手电筒");
+            isFlash = false;
+        }else{
+            Camera.Parameters mParameterss = camera.getParameters();
+            mParameterss.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(mParameterss);
+            mButtonOpenOrClose.setText("打开手电筒");
+            isFlash = true;
+        }
+    }
 
+    // 应用退出就关闭
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        camera.release();	// 释放手电筒
     }
 }

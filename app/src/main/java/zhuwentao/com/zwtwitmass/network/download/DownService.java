d7b798089c867f6b2b15b7c7e8d84594a99c4c2e
zhuwentao <215069587@qq.com>
2017-05-29 23:44:23 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/network/download/DownService.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/network/download/DownService.java
@@ -2,6 +2,7 @@ package zhuwentao.com.zwtwitmass.network.download;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -14,11 +15,25 @@ public class DownService extends Service{
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new MyBinder();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
     }
+
+
+    public class MyBinder extends Binder {
+        /**
+         * 得到一个Binder对象
+         *
+         * @return
+         */
+        public DownService getService() {
+            return DownService.this;
+        }
+    }
+
+
 }

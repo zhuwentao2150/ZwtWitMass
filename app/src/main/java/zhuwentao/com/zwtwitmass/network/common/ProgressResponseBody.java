2ea2b716b0b4955e75ff1002c021b33e452069e2
zhuwentao <215069587@qq.com>
2017-06-07 23:43:48 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/network/common/ProgressResponseBody.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/network/common/ProgressResponseBody.java
@@ -16,52 +16,52 @@ import zhuwentao.com.zwtwitmass.network.callback.ProgressListener;
  */
 public class ProgressResponseBody extends ResponseBody {
 
-	private final ResponseBody responseBody;
-	private final ProgressListener listener;
-	private BufferedSource bufferedSource;
+    private final ResponseBody responseBody;
+    private final ProgressListener listener;
+    private BufferedSource bufferedSource;
 
-	public ProgressResponseBody(ResponseBody responseBody,
-			ProgressListener listener) {
-		super();
-		this.responseBody = responseBody;
-		this.listener = listener;
-	}
+    public ProgressResponseBody(ResponseBody responseBody,
+                                ProgressListener listener) {
+        super();
+        this.responseBody = responseBody;
+        this.listener = listener;
+    }
 
-	@Override
-	public long contentLength() {
-		return responseBody.contentLength();
-	}
+    @Override
+    public long contentLength() {
+        return responseBody.contentLength();
+    }
 
-	@Override
-	public MediaType contentType() {
-		return responseBody.contentType();
-	}
+    @Override
+    public MediaType contentType() {
+        return responseBody.contentType();
+    }
 
-	@Override
-	public BufferedSource source() {
-		if (null == bufferedSource) {
-			bufferedSource = Okio.buffer(source(responseBody.source()));
-		}
-		return bufferedSource;
-	}
+    @Override
+    public BufferedSource source() {
+        if (null == bufferedSource) {
+            bufferedSource = Okio.buffer(source(responseBody.source()));
+        }
+        return bufferedSource;
+    }
 
-	private Source source(Source source) {
-		return new ForwardingSource(source) {
-			long totalBytesRead = 0L;
+    private Source source(Source source) {
+        return new ForwardingSource(source) {
+            long totalBytesRead = 0L;
 
-			@Override
-			public long read(Buffer sink, long byteCount) throws IOException {
-				long bytesRead = super.read(sink, byteCount);
+            @Override
+            public long read(Buffer sink, long byteCount) throws IOException {
+                long bytesRead = super.read(sink, byteCount);
 
-				// 增加当前读取的字节数，如果读取完成了bytesRead会返回-1
-				totalBytesRead += bytesRead != -1 ? bytesRead : 0;
+                // 增加当前读取的字节数，如果读取完成了bytesRead会返回-1
+                totalBytesRead += bytesRead != -1 ? bytesRead : 0;
 
-				// 回调，如果contentLength()不知道长度，会返回-1
-				listener.onProgress(totalBytesRead,
-						responseBody.contentLength(), bytesRead == -1);
-				return bytesRead;
-			}
-		};
-	}
+                // 回调，如果contentLength()不知道长度，会返回-1
+                listener.onProgress(totalBytesRead,
+                        responseBody.contentLength(), bytesRead == -1);
+                return bytesRead;
+            }
+        };
+    }
 
 }

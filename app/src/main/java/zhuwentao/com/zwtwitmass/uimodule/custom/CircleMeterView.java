005141a4c8125a084d2fee98e91f0ac4dfff9493
zhuwentao <215069587@qq.com>
2017-06-08 23:11:35 +0800

    usually commit

---
+++ app/src/main/java/zhuwentao/com/zwtwitmass/uimodule/custom/CircleMeterView.java
@@ -0,0 +1,24 @@
+package zhuwentao.com.zwtwitmass.uimodule.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 圆形仪表盘
+ * Created by zhuwentao on 2017-06-08.
+ */
+public class CircleMeterView extends View{
+
+    public CircleMeterView(Context context) {
+        super(context);
+    }
+
+    public CircleMeterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleMeterView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

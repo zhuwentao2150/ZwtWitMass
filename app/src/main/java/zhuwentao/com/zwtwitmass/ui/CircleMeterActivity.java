11fc4c0da51a537c4952a31b6ef6588610ac16b3
zhuwentao <215069587@qq.com>
2017-06-11 23:37:20 +0800

    usually commit

---
+++ app/src/main/java/zhuwentao/com/zwtwitmass/ui/CircleMeterActivity.java
@@ -0,0 +1,34 @@
+package zhuwentao.com.zwtwitmass.ui;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+import zhuwentao.com.zwtwitmass.R;
+import zhuwentao.com.zwtwitmass.uimodule.BaseActivity;
+import zhuwentao.com.zwtwitmass.uimodule.custom.CircleMeterView;
+
+/**
+ *
+ * 圆形仪表盘
+ * Created by zhuwentao on 2017-06-11.
+ */
+public class CircleMeterActivity extends BaseActivity {
+
+
+    private SeekBar indexSb;
+
+    private CircleMeterView circleMeter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_circle_meter);
+
+        initUI();
+    }
+
+    private void initUI() {
+        indexSb = (SeekBar) findViewById(R.id.sb_index_position);
+        circleMeter = (CircleMeterView) findViewById(R.id.cmv_meter);
+    }
+}

99496d1ada737f2bff9f289644f61e7a676ec597
zhuwentao <215069587@qq.com>
2017-07-08 00:53:29 +0800

    usually commit

--- app/src/main/java/zhuwentao/com/zwtwitmass/utils/FileUtil.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/utils/FileUtil.java
@@ -1,5 +1,7 @@
 package zhuwentao.com.zwtwitmass.utils;
 
+import java.io.File;
+
 /**
  * 文件操作类
  * Created by zhuwentao on 2017-07-07.
@@ -8,8 +10,12 @@ public class FileUtil {
 
     public static boolean isFile(String path){
 
+        File file = new File(path);
+        if (file.exists()) {
+            return true;
+        }
 
-        return true;
+        return false;
     }
 
 }

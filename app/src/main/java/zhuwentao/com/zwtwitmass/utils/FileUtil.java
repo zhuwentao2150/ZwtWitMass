7820dd8a1ab3cf8158795f976722455b5136a2c3
zhuwentao <215069587@qq.com>
2017-07-07 23:52:57 +0800

    usually commit

---
+++ app/src/main/java/zhuwentao/com/zwtwitmass/utils/FileUtil.java
@@ -0,0 +1,15 @@
+package zhuwentao.com.zwtwitmass.utils;
+
+/**
+ * 文件操作类
+ * Created by zhuwentao on 2017-07-07.
+ */
+public class FileUtil {
+
+    public static boolean isFile(String path){
+
+
+        return true;
+    }
+
+}

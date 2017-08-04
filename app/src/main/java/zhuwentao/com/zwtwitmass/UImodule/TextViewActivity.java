fb7fb3ef015177573db754aaa7315808a8fc2f13
zhuwentao <215069587@qq.com>
2017-08-04 23:32:18 +0800

    usually commit Time

--- app/src/main/java/zhuwentao/com/zwtwitmass/UImodule/TextViewActivity.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/UImodule/TextViewActivity.java
@@ -7,6 +7,12 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+
 import java.text.Collator;
 import java.util.Arrays;
 import java.util.Collections;
@@ -38,16 +44,47 @@ public class TextViewActivity extends AppCompatActivity {
         // 汉字转拼音
         String str = "重庆";
         String strPull = TextPinyinUtil.getInstance().getPinyin(str);
+
+        // 输出单个字符的所有拼音
+        char cStr = '略';
+        String[] cStrHY = PinyinHelper.toHanyuPinyinStringArray(cStr);
+        for (int i = 0; i < cStrHY.length; i++) {
+            Log.i("pinyin", cStrHY[i]);
+        }
+
+
+//        PinyinHelper.toTongyongPinyinStringArray();
+//        PinyinHelper.toWadeGilesPinyinStringArray();
+//        PinyinHelper.toYalePinyinStringArray();
+//        PinyinHelper.toGwoyeuRomatzyhStringArray();
+//        PinyinHelper.toMPS2PinyinStringArray();
+//        PinyinHelper.toHanyuPinyinStringArray();
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setToneType(HanyuPinyinToneType.WITH_TONE_MARK);
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        try {
+            String[] cStrFormatHY = PinyinHelper.toHanyuPinyinStringArray(cStr, format);
+            for (int i = 0; i < cStrFormatHY.length; i++) {
+                Log.e("pinyinFormat", cStrFormatHY[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
         mAutotv.setText(strPull);
 
         // 对汉字进行排序
-        Comparator<Object> com= Collator.getInstance(java.util.Locale.CHINA);
-        String[] newArray={"中山","华山","西门子","阿里巴巴","阳江","南京","重庆","北京","安阳","北方","电控系统","朝阳"};
+        Comparator<Object> com = Collator.getInstance(java.util.Locale.CHINA);
+        String[] newArray = {"中山", "华山", "西门子", "阿里巴巴", "阳江", "南京", "重庆", "北京", "安阳", "北方", "电控系统", "朝阳"};
         List<String> list = Arrays.asList(newArray);
         Collections.sort(list, com);
-        for(String i:list){
-            Log.e("排序", "排序后：" + i);
-        }
+//        for(String i:list){
+//            Log.e("排序", "排序后：" + i);
+//        }
 
 
         mInputEdt.addTextChangedListener(new TextWatcher() {

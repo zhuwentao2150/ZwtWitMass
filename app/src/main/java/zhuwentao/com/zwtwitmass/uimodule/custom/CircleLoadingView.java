33ddd9be81fc7d1c0bd2973bdd948d81c4d526d1
zhuwentao <215069587@qq.com>
2017-08-23 21:52:17 +0800

    usually commit Time

--- app/src/main/java/zhuwentao/com/zwtwitmass/uimodule/custom/CircleLoadingView.java
+++ app/src/main/java/zhuwentao/com/zwtwitmass/uimodule/custom/CircleLoadingView.java
@@ -2,6 +2,7 @@ package zhuwentao.com.zwtwitmass.uimodule.custom;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,6 +11,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
+import zhuwentao.com.zwtwitmass.R;
 import zhuwentao.com.zwtwitmass.utils.DensityUtil;
 
 /**
@@ -20,8 +22,14 @@ public class CircleLoadingView extends View {
 
     private Context mContext;
 
-    // 普通画笔
-    private Paint mPaint;
+    // 刻度画笔
+    private Paint mScalePaint;
+
+    // 原点画笔
+    private Paint mDotPaint;
+
+    // 文字画笔
+    private Paint mTextPaint;
 
     // 当前进度
     private int progress = 0;
@@ -31,38 +39,71 @@ public class CircleLoadingView extends View {
      */
     public float mDotProgress;
 
-    /**
-     * 圆的直径
-     */
-    private float raduis = 180;
-
     // View宽
     private int mWidth;
     // View高
     private int mHeight;
 
+    // 圆半径
+    private float raduis = 180;
+
+    private int indexColor;
+
+    private int baseColor;
+
+    private int dotColor;
+
+    private int textSize;
+
+    private int textColor;
 
     public CircleLoadingView(Context context) {
-        super(context);
-        initUI();
+        this(context, null);
     }
 
     public CircleLoadingView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initUI();
+        this(context, attrs, 0);
     }
 
     public CircleLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray tya = context.obtainStyledAttributes(attrs, R.styleable.CircleLoading);
+        baseColor = tya.getColor(R.styleable.CircleLoading_baseColor, Color.LTGRAY);
+        indexColor = tya.getColor(R.styleable.CircleLoading_indexColor, Color.BLUE);
+        textColor = tya.getColor(R.styleable.CircleLoading_textColor, Color.BLUE);
+        dotColor = tya.getColor(R.styleable.CircleLoading_dotColor, Color.RED);
+        textSize = tya.getDimensionPixelSize(R.styleable.CircleLoading_textSize, 36);
+        tya.recycle();
+
         initUI();
     }
 
     private void initUI() {
         mContext = getContext();
-        // 设置圆的直径
-        raduis = DensityUtil.dip2px(mContext, raduis);
 
-        mPaint = new Paint();
+
+        // 刻度画笔
+        mScalePaint = new Paint();
+        mScalePaint.setAntiAlias(true);
+        mScalePaint.setStrokeWidth(DensityUtil.dip2px(mContext, 1));
+        mScalePaint.setStrokeCap(Paint.Cap.ROUND);
+        mScalePaint.setColor(baseColor);
+        mScalePaint.setStyle(Paint.Style.STROKE);
+
+        // 小圆点画笔
+        mDotPaint = new Paint();
+        mDotPaint.setAntiAlias(true);
+        mDotPaint.setColor(dotColor);
+        mDotPaint.setStrokeWidth(DensityUtil.dip2px(mContext, 1));
+        mDotPaint.setStyle(Paint.Style.FILL);
+
+        // 文字画笔
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setColor(textColor);
+        mTextPaint.setTextSize(textSize);
+        mTextPaint.setStrokeWidth(DensityUtil.dip2px(mContext, 1));
+        mTextPaint.setStyle(Paint.Style.FILL);
 
 
 //        // 绘图线程
@@ -96,67 +137,47 @@ public class CircleLoadingView extends View {
      * 画刻度
      */
     private void drawArcScale(Canvas canvas) {
-        mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(DensityUtil.dip2px(mContext, 1));
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setColor(Color.LTGRAY);
-        mPaint.setStyle(Paint.Style.STROKE);
-
         canvas.save();
-        int pointX = getHeight() / 2;
-        int pointY = getWidth() / 2;
         for (int i = 0; i < 100; i++) {
             if (progress > i) {
-                mPaint.setColor(Color.BLUE);
-                canvas.drawLine(getWidth() / 2, 0, pointX, DensityUtil.dip2px(mContext, 10), mPaint);
+                mScalePaint.setColor(Color.BLUE);
+                canvas.drawLine(mWidth / 2, 0, mHeight / 2, DensityUtil.dip2px(mContext, 10), mScalePaint);
             } else {
-                mPaint.setColor(Color.LTGRAY);
-                canvas.drawLine(getWidth()/2, 0, pointX, DensityUtil.dip2px(mContext, 10), mPaint);
+                mScalePaint.setColor(Color.LTGRAY);
+                canvas.drawLine(mWidth / 2, 0, mHeight / 2, DensityUtil.dip2px(mContext, 10), mScalePaint);
             }
-            // 旋转的度数 = 100 % 360
-            canvas.rotate(3.6f, pointX, pointY);
+            // 旋转的度数 = 100 / 360
+            canvas.rotate(3.6f, mWidth / 2, mHeight / 2);
         }
         canvas.restore();
     }
 
     /**
      * 画内部数值
+     *
      * @param canvas
      */
     private void drawTextValue(Canvas canvas) {
         canvas.save();
 
-        mPaint.setColor(Color.BLUE);
-        mPaint.setTextSize(36);
-        mPaint.setStrokeWidth(2);
-        mPaint.setStyle(Paint.Style.FILL);
-
         String showValue = String.valueOf(progress);
         Rect textBound = new Rect();
-        mPaint.getTextBounds(showValue, 0, showValue.length(), textBound);    // 获取文字的矩形范围
+        mTextPaint.getTextBounds(showValue, 0, showValue.length(), textBound);    // 获取文字的矩形范围
         float textWidth = textBound.right - textBound.left;  // 获得文字宽
         float textHeight = textBound.bottom - textBound.top; // 获得文字高
 
-        canvas.drawText(showValue, getWidth()/2 - textWidth/2, getHeight()/2 + textHeight/2, mPaint);
+        canvas.drawText(showValue, mWidth / 2 - textWidth / 2, mHeight / 2 + textHeight / 2, mTextPaint);
 
         canvas.restore();
     }
 
     /**
      * 画外部旋转小圆点
-     * @param canvas
      */
     private void drawRotateDot(final Canvas canvas) {
         canvas.save();
-
-        mPaint.setColor(Color.RED);
-        mPaint.setStrokeWidth(1);
-        mPaint.setStyle(Paint.Style.FILL);
-
         canvas.rotate(mDotProgress * 3.6f, mWidth / 2, mHeight / 2);
-        canvas.drawCircle(getWidth()/2, DensityUtil.dip2px(mContext, 10) + DensityUtil.dip2px(mContext, 5), DensityUtil.dip2px(mContext, 3), mPaint);
-
-
+        canvas.drawCircle(mWidth / 2, DensityUtil.dip2px(mContext, 10) + DensityUtil.dip2px(mContext, 5), DensityUtil.dip2px(mContext, 3), mDotPaint);
         canvas.restore();
     }
 
@@ -165,7 +186,7 @@ public class CircleLoadingView extends View {
      */
     public void start() {
         final ValueAnimator animator = ValueAnimator.ofFloat(0, 100);
-        animator.setDuration(2000);
+        animator.setDuration(1500);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setRepeatMode(ValueAnimator.RESTART);
         // 设置旋转速率
@@ -174,21 +195,19 @@ public class CircleLoadingView extends View {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 // 只能转成float类型
-                mDotProgress =  (Float) animation.getAnimatedValue();
+                mDotProgress = (Float) animation.getAnimatedValue();
                 invalidate();
             }
         });
         animator.start();
     }
 
-
-
-
     /**
      * 设置进度
+     *
      * @param progress
      */
-    public void setProgress(int progress){
+    public void setProgress(int progress) {
         this.progress = progress;
         invalidate();
     }

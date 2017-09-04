e2d0943d6755e8ae81bfcaf88d4a12492371233d
zhuwentao <215069587@qq.com>
2017-09-04 23:24:21 +0800

    usually commit Time

---
+++ app/src/main/java/zhuwentao/com/zwtwitmass/ui/FragmentLinkActivity.java
@@ -0,0 +1,19 @@
+package zhuwentao.com.zwtwitmass.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zhuwentao.com.zwtwitmass.R;
+
+
+/**
+ * Activity 与 Fragment的多种通信方式
+ */
+public class FragmentLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_fragment_link);
+    }
+}
